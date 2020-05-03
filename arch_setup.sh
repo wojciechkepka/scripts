9d310ede906cb976708a0126d17f2af34a7a7b80
wojciechkepka <wojtek.kepka@protonmail.com>
2020-05-03 18:53:52 +0200

    [arch_setup] Trap ctrl_c, add user confirm function

--- arch_setup.sh
+++ arch_setup.sh
@@ -84,6 +84,31 @@ AUR_PACKAGES=(
 )
 
 ################################################################################
+trap ctrl_c INT
+function ctrl_c() {
+	echo "Exiting..."
+	exit 1
+}
+ask() {
+	local msg="$1"
+	shift
+	echo "$msg y/n"
+	while true
+	do
+		read -n 1 -s c
+		case "$c" in
+			"y")
+				"$@"
+				break
+				;;
+			"n")
+				break
+				;;
+			*)
+				;;
+		esac
+	done
+}
 notify() {
 	local msg="$1"
 	echo "################################################################################"
