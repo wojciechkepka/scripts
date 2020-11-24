c5294fb472c45b23bea6bf40f640649150144bea
wojciechkepka <wojtek.kepka@protonmail.com>
2020-11-24 18:09:15 +0100

    Add initial render command

--- templater/cli.py
+++ templater/cli.py
@@ -11,13 +11,18 @@ CLI tool to manage themes and configurations with multiple color schemes.
 import json
 import argparse
 import sys
+from pathlib import Path
+from typing import Dict
 
 sys.path.append("../")
 from util import catch_errs
+from templater import Templater
 
 ################################################################################
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ classes ~~~~~~~~~~~|
 ################################################################################
+
+Variables = Dict[str, Dict[str, str]]
 
 
 class TempalterCli(object):
@@ -26,20 +31,36 @@ class TempalterCli(object):
         parser = argparse.ArgumentParser("templater", description="CLI tool to manage themes and color schemes.")
         subparsers = parser.add_subparsers(dest="command", help="Command to run", required=True)
 
-        set_parser = subparsers.add_parser("set", help="Set a theme")
-        set_parser.add_argument("theme", nargs=1, help="Theme to set")
+        render_parser = subparsers.add_parser("render", help="Render a file")
+        render_parser.add_argument("file", nargs=1, type=Path, help="File to render")
+        render_parser.add_argument(
+            "variables", nargs=1, type=Path, help="File containing a map of values for each theme"
+        )
+        render_parser.add_argument("theme", nargs=1, type=str, help="Theme that will be used to render this file")
 
         return parser
 
     def __init__(self):
         self.args = self.__parser().parse_args()
 
-    def __set(self):
-        pass
+    @staticmethod
+    def _read_variables_file(location: Path) -> Variables:
+        with location.open() as f:
+            var = json.load(f)
+            if isinstance(var, dict):
+                return var
+
+        return {}
+
+    def __render(self):
+        with open(self.args.file[0], "r") as f:
+            text = f.read()
+            variables = self._read_variables_file(self.args.variables[0])
+            print(Templater(text, variables[self.args.theme[0]]).render())
 
     def _process_args(self):
-        if self.args.command == "set":
-            self.__set()
+        if self.args.command == "render":
+            self.__render()
 
     def main(self):
         catch_errs(self._process_args)
