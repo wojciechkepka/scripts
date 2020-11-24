4640bb1ccf0d48b593d79157e1fe303a39881460
wojciechkepka <wojtek.kepka@protonmail.com>
2020-11-24 05:42:08 +0100

    Add templater.py

---
+++ setup/templater.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+templater
+
+CLI tool to manage themes and configurations with multiple color schemes.
+"""
+################################################################################
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ imports ~~~~~~~~~~~|
+################################################################################
+
+import json
+import argparse
+from util import catch_errs
+
+################################################################################
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ classes ~~~~~~~~~~~|
+################################################################################
+
+
+class Tempalter(object):
+    @staticmethod
+    def __parser():
+        parser = argparse.ArgumentParser("templater", description="CLI tool to manage themes and color schemes.")
+        subparsers = parser.add_subparsers(dest="command", help="Command to run", required=True)
+
+        set_parser = subparsers.add_parser("set", help="Set a theme")
+        set_parser.add_argument("theme", nargs=1, help="Theme to set")
+
+        return parser
+
+    def __init__(self):
+        self.args = self.__parser().parse_args()
+
+    def __set(self):
+        pass
+
+    def _process_args(self):
+        if self.args.command == "set":
+            self.__set()
+
+    def main(self):
+        catch_errs(self._process_args())
+
+
+################################################################################
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ main ~~~~~~~~~~~~~~|
+################################################################################
+
+
+if __name__ == "__main__":
+    Tempalter().main()
